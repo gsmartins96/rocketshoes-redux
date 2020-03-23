b637bf869b7d527baf8122d2e28a0377a4a362eb
Gabriel dos Santos Martins <gsmartins96@hotmail.com>
2020-03-23 17:15:04 -0300

    :construction: create cart page and your style

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
 export default function Card() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-prophecy-9-masculino/20/D16-5355-120/D16-5355-120_zoom2.jpg?ts=1583144798&ims=326x"
+                alt="Mizuno Prophecy"
+              />
+            </td>
+            <td>
+              <strong>Mizuno Prophecy Wave 9</strong>
+              <span>R$ 1.299,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 1.599,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+        <Total>
+          <span>Total: </span>
+          <strong>R$ 3.699,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }

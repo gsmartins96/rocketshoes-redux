2fc662057ef289ff3058b5218d72c5a52cff65da
Gabriel dos Santos Martins <gsmartins96@hotmail.com>
2020-03-23 15:47:43 -0300

    :sparkles: adding globalstyles
    adding global styles, config pages, routes, ans basics for web application

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyles from '../src/styles/global';
 
 export default function App() {
   return (
     <BrowserRouter>
       {/* <Header /> */}
       <Routes />
+      <GlobalStyles />
     </BrowserRouter>
   );
 }
